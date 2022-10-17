4d8bc5a64f38eef1643d9382eb402e70faecdce7
nirumiantcev <fed.nick2013@yandex.ru>
2022-10-17 17:11:17 +0300

    add pages

---
+++ views/pages/not_found.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Helpers\Page;
+
+?>
+
+<!doctype html>
+<html lang="en">
+<?php
+Page::part('head');
+?>
+<body>
+<?php
+Page::part('navbar');
+?>
+<div class="container">
+    <h1 class="mt-3">404: Page not found</h1>
+</div>
+<script src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
